f53309a4933f7fc28a9a540bc1f468e9ffb79a66
DKAvocad00 <kurilodaniilds@gmail.com>
2024-10-24 18:08:57 +0300

    changes in smart search

--- apartments.php
+++ apartments.php
@@ -233,7 +233,7 @@ JOIN (
     
     if ($isActive && $isClient) {
         $query .= " GROUP BY a.landlordID) AS landlord_stats ON a.landlordID = landlord_stats.landlordID
-        ORDER BY landlord_stats.favorite_count DESC, landlord_stats.application_count DESC;";
+        ORDER BY landlord_stats.application_count  DESC, landlord_stats.favorite_count DESC;";
     }
     
     
